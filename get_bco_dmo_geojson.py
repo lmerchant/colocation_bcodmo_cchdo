22dfd9cfa674672621904707b7f361fd4dcd5453
Lynne Merchant <merchant.lynne@gmail.com>
2026-02-15 14:29:53 -0800

    update formatting

--- get_bco_dmo_geojson.py
+++ get_bco_dmo_geojson.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import pandas as pd
 import itertools
+import time
 
 import search_pages_meta_data as meta
 import check_if_ctd_data as ctd
@@ -14,7 +15,7 @@ import simplify
 
 def remove_duplicate_features(geojson):
 
-    features = geojson['features']
+    features = geojson["features"]
     new_features = []
 
     features_set = set()
@@ -25,18 +26,18 @@ def remove_duplicate_features(geojson):
 
         if str_feature in features_set:
             continue
-        else: 
+        else:
             features_set.add(str_feature)
             new_features.append(feature)
 
-    geojson['features'] = new_features
+    geojson["features"] = new_features
 
     return geojson
 
 
 def convert_json_to_geojson(bco_dmo_json):
 
-    coordinates = bco_dmo_json['geometry']['track']['coordinates']
+    coordinates = bco_dmo_json["geometry"]["track"]["coordinates"]
 
     features = []
 
@@ -87,15 +88,15 @@ def simplify_lon_lat_list(lon_lat_list):
 def get_lon_lat_list(df, pressure_col_index, latitude_col_index, longitude_col_index):
 
     # remove rows where cell has nan value
-    df = df[df.iloc[:,pressure_col_index].notnull()]
-    df = df[df.iloc[:,latitude_col_index].notnull()]
-    df = df[df.iloc[:,longitude_col_index].notnull()]
+    df = df[df.iloc[:, pressure_col_index].notnull()]
+    df = df[df.iloc[:, latitude_col_index].notnull()]
+    df = df[df.iloc[:, longitude_col_index].notnull()]
 
     # remove rows where nd in lat or lon column
-    df = df[ df.iloc[:,latitude_col_index] != 'nd']
-    df = df[ df.iloc[:,longitude_col_index] != 'nd']
+    df = df[df.iloc[:, latitude_col_index] != "nd"]
+    df = df[df.iloc[:, longitude_col_index] != "nd"]
 
-    df = df.iloc[:,[latitude_col_index, longitude_col_index]].copy()
+    df = df.iloc[:, [latitude_col_index, longitude_col_index]].copy()
 
     df = df.apply(pd.to_numeric)
     df = df.dropna()
@@ -106,7 +107,7 @@ def get_lon_lat_list(df, pressure_col_index, latitude_col_index, longitude_col_i
 
     for index, row in df.iterrows():
 
-        lon_lat_pair = df.iloc[index, [1,0]].values.tolist()
+        lon_lat_pair = df.iloc[index, [1, 0]].values.tolist()
         lon_lat_list.append(lon_lat_pair)
 
     return lon_lat_list
@@ -115,7 +116,7 @@ def get_lon_lat_list(df, pressure_col_index, latitude_col_index, longitude_col_i
 def get_platforms(dataset_soup):
 
     # <a href="/platform/53992">R/V Endeavor</a>
-    links = dataset_soup.find_all('a', href = re.compile(r'/platform/\d+'))
+    links = dataset_soup.find_all("a", href=re.compile(r"/platform/\d+"))
 
     platforms = [link.string for link in links]
 
@@ -124,8 +125,8 @@ def get_platforms(dataset_soup):
 
 def get_deployments(dataset_soup):
 
-    # <a href="/deployment/57739">EN198</a> 
-    links = dataset_soup.find_all('a', href = re.compile(r'/deployment/\d+'))
+    # <a href="/deployment/57739">EN198</a>
+    links = dataset_soup.find_all("a", href=re.compile(r"/deployment/\d+"))
 
     deployments = [link.string for link in links]
 
@@ -151,10 +152,10 @@ def get_temporal_coverage(dataset_soup):
 def get_start_date(dataset_soup):
 
     # <td class="views-field views-field-field-deployment-start-date nowrap" >
-    #<span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="1989-06-28T04:00:00-04:00">1989-06-28</span>          
-    #</td>
-    td = dataset_soup.find('td', {'class': 'views-field-field-deployment-start-date'})
-    span = td.find('span')
+    # <span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="1989-06-28T04:00:00-04:00">1989-06-28</span>
+    # </td>
+    td = dataset_soup.find("td", {"class": "views-field-field-deployment-start-date"})
+    span = td.find("span")
     start_date = span.string
 
     return start_date
@@ -169,18 +170,20 @@ def get_start_end_dates(dataset_soup):
 
     if temporal_coverage:
 
-        dates = temporal_coverage.split('/')
+        dates = temporal_coverage.split("/")
 
         start_date = dates[0]
         end_date = dates[1]
 
     else:
-        end_date = None   
+        end_date = None
 
     return start_date, end_date
 
 
-def create_bco_dmo_json(index, dataset_id, titles, investigators, dataset_soup, lon_lat_list):
+def create_bco_dmo_json(
+    index, dataset_id, titles, investigators, dataset_soup, lon_lat_list
+):
 
     bco_dmo_json = {}
 
@@ -195,12 +198,12 @@ def create_bco_dmo_json(index, dataset_id, titles, investigators, dataset_soup, 
     bco_dmo_json["end_date"] = end_date
 
     bco_dmo_json["deployments"] = get_deployments(dataset_soup)
-    bco_dmo_json["platforms"] = get_platforms(dataset_soup)        
+    bco_dmo_json["platforms"] = get_platforms(dataset_soup)
 
-    bco_dmo_json["geometry"] = {"track":{}}
-    bco_dmo_json["geometry"]["track"] = {"coordinates":{}}
-    bco_dmo_json['geometry']['track']['coordinates'] = lon_lat_list
-    bco_dmo_json['geometry']['track']['type'] = "LineString"
+    bco_dmo_json["geometry"] = {"track": {}}
+    bco_dmo_json["geometry"]["track"] = {"coordinates": {}}
+    bco_dmo_json["geometry"]["track"]["coordinates"] = lon_lat_list
+    bco_dmo_json["geometry"]["track"]["type"] = "LineString"
 
     return bco_dmo_json
 
@@ -208,51 +211,40 @@ def create_bco_dmo_json(index, dataset_id, titles, investigators, dataset_soup, 
 def save_geojson(dataset_id, geojson):
 
     # Save json to file
-    filename = str(dataset_id) + '.json'
+    filename = str(dataset_id) + ".json"
 
-    filepath = './output_geojson/' + filename
+    filepath = "./output_geojson/" + filename
 
-    with open(filepath, 'w') as f:
-        json.dump(geojson, f, indent=4, sort_keys=True) 
-
-
-def get_geojson_url(dataset_id):
-
-    #return f"https://erddap.bco-dmo.org/erddap/tabledap/bcodmo_dataset_{str(dataset_id)}.geoJson"
-
-    return f"https://erddap.bco-dmo.org/erddap/tabledap/bcodmo_dataset_{str(dataset_id)}.geoJson"
+    with open(filepath, "w") as f:
+        json.dump(geojson, f, indent=4, sort_keys=True)
 
 
 def get_geojson(dataset_id, processing_log):
 
-    url = get_geojson_url(dataset_id)
+    url = f"https://erddap.bco-dmo.org/erddap/tabledap/bcodmo_dataset_{str(dataset_id)}.geoJson"
 
     r = requests.get(url)
+    time.sleep(5)
 
     if r.status_code != 200:
 
-        with open(processing_log, 'a+') as f:
+        with open(processing_log, "a+") as f:
             text = f"No CTD geoJSON file at dataset id: {dataset_id}\n"
-            f.write(text)   
-                     
+            f.write(text)
+
         return None
 
     return r.json()
 
 
-def get_dataset_url(dataset_id):
-
-    url = f'http://www.bco-dmo.org/dataset/{dataset_id}'
-    return url    
-
-
 def get_dataset_soup(dataset_id, processing_log, page, number_of_datasets_per_page):
 
-    url = get_dataset_url(dataset_id)
+    url = f"http://www.bco-dmo.org/dataset/{dataset_id}"
 
     try:
         response = requests.get(url)
-        dataset_soup = BeautifulSoup(response.text, 'html.parser')
+        time.sleep(5)
+        dataset_soup = BeautifulSoup(response.text, "html.parser")
 
     except requests.exceptions.RequestException as e:
         # response doesn't exist
@@ -260,14 +252,14 @@ def get_dataset_soup(dataset_id, processing_log, page, number_of_datasets_per_pa
 
         # Can't reach data page
         # Log it
-        with open(processing_log, 'a+') as f:
+        with open(processing_log, "a+") as f:
             text = f"Site not reached for dataset id: {dataset_id} on page {page + 1} with {number_of_datasets_per_page} data sets on page\n"
             f.write(text)
 
     # No dataset available
-    if dataset_soup and not dataset_soup(text='Data URL:'):
+    if dataset_soup and not dataset_soup(text="Data URL:"):
 
-        with open(processing_log, 'a+') as f:
+        with open(processing_log, "a+") as f:
             text = f"No data set for id: {dataset_id} on page {page + 1} with {number_of_datasets_per_page} data sets on page\n"
             f.write(text)
 
@@ -276,21 +268,33 @@ def get_dataset_soup(dataset_id, processing_log, page, number_of_datasets_per_pa
     return dataset_soup
 
 
-def get_bco_dmo_json(index, page, number_of_datasets_per_page, dataset_id, titles, investigators, processing_log):
+def get_bco_dmo_json(
+    index,
+    page,
+    number_of_datasets_per_page,
+    dataset_id,
+    titles,
+    investigators,
+    processing_log,
+):
 
     # TODO: process zip files
 
     bco_dmo_json = {}
 
-    dataset_soup = get_dataset_soup(dataset_id, processing_log, page, number_of_datasets_per_page)
+    dataset_soup = get_dataset_soup(
+        dataset_id, processing_log, page, number_of_datasets_per_page
+    )
 
     if not dataset_soup:
         # No BCO-DMO json to create
         return {}
 
     # Put data into a dataframe
-    dataset_df = ctd.put_dataset_into_dataframe(dataset_id, processing_log, page, index, number_of_datasets_per_page)
-    
+    dataset_df = ctd.put_dataset_into_dataframe(
+        dataset_id, processing_log, page, index, number_of_datasets_per_page
+    )
+
     if dataset_df is None:
         # Can't create dataframe
         return {}
@@ -298,42 +302,51 @@ def get_bco_dmo_json(index, page, number_of_datasets_per_page, dataset_id, title
     # Check dataframe to see if CTD data
     column_names = dataset_df.columns
 
-    is_ctd, pressure_col_index, latitude_col_index, longitude_col_index = ctd.check_column_names_for_ctd(column_names)
+    (
+        is_ctd,
+        pressure_col_index,
+        latitude_col_index,
+        longitude_col_index,
+    ) = ctd.check_column_names_for_ctd(column_names)
 
     if is_ctd:
 
-        lon_lat_list = get_lon_lat_list(dataset_df, pressure_col_index, latitude_col_index, longitude_col_index)
+        lon_lat_list = get_lon_lat_list(
+            dataset_df, pressure_col_index, latitude_col_index, longitude_col_index
+        )
 
-        # TODO: Fix so only simplify if file has more 
-        # than say 100 points
-        #lon_lat_list = simplify_lon_lat_list(lon_lat_list)
+        # TODO: Fix so only simplify if file has more
+        # than say 500 points
+        # lon_lat_list = simplify_lon_lat_list(lon_lat_list)
 
         ctd_files_log = "./ctd_files_log.txt"
 
-        with open(ctd_files_log, 'a+') as f:
+        with open(ctd_files_log, "a+") as f:
             text = f"File is CTD at dataset id: {dataset_id}\n"
             f.write(text)
 
         # If the file is ctd, get geojson data if it exists
         geojson = get_geojson(dataset_id, processing_log)
 
-        if geojson:            
+        if geojson:
             save_geojson(dataset_id, geojson)
 
-            geojson_files_log = './geojson_ctd_files.txt'
-            with open(geojson_files_log, 'a+') as f:
+            geojson_files_log = "./geojson_ctd_files.txt"
+            with open(geojson_files_log, "a+") as f:
                 text = f"File is geojson CTD at dataset id: {dataset_id}\n"
-                f.write(text)  
+                f.write(text)
 
     else:
 
-        with open(processing_log, 'a+') as f:
+        with open(processing_log, "a+") as f:
             text = f"No ctd data for id: {dataset_id} on page {page + 1} with {number_of_datasets_per_page} data sets on page\n"
             f.write(text)
 
         return {}
 
-    bco_dmo_json = create_bco_dmo_json(index, dataset_id, titles, investigators, dataset_soup, lon_lat_list)
+    bco_dmo_json = create_bco_dmo_json(
+        index, dataset_id, titles, investigators, dataset_soup, lon_lat_list
+    )
 
     return bco_dmo_json
 
@@ -352,20 +365,32 @@ def main():
 
     page_dataset_ids, titles, investigators = meta.get_metadata(search_start_page)
 
-
     for index, (page, dataset_id) in enumerate(page_dataset_ids):
 
-        # dataset 3458 and 527438 timed out. (Error 502) 
+        # TODO
+        # add delay in requesting files
+
+        # dataset 3458 and 527438 timed out. (Error 502)
         # Not CTD file though
-        if dataset_id in ['3458', '527438', '2467']:
-        # if dataset_id in ['3355']:   
-             continue
-        else:
-             pass
+        # if dataset_id in ['3458', '527438', '2467']:
+        # # if dataset_id in ['3355']:
+        #      continue
+        # else:
+        #      pass
 
-        print(f"Processing dataset id {dataset_id} on page {page + 1}: number {index+1} out of {len(page_dataset_ids)}")
+        print(
+            f"Processing dataset id {dataset_id} on page {page + 1}: number {index+1} out of {len(page_dataset_ids)}"
+        )
 
-        bco_dmo_json = get_bco_dmo_json(index, page, number_of_datasets_per_page, dataset_id, titles, investigators, processing_log)     
+        bco_dmo_json = get_bco_dmo_json(
+            index,
+            page,
+            number_of_datasets_per_page,
+            dataset_id,
+            titles,
+            investigators,
+            processing_log,
+        )
 
         if not bco_dmo_json:
             continue
@@ -376,15 +401,14 @@ def main():
         bco_dmo_geojson = remove_duplicate_features(bco_dmo_geojson)
 
         # Save json to file
-        #filename = str(dataset_id) + '-' + str(page) + '.json'
-        filename = str(dataset_id) + '.json'
+        # filename = str(dataset_id) + '-' + str(page) + '.json'
+        filename = str(dataset_id) + ".json"
 
-        filepath = './output/' + filename
+        filepath = "./output/" + filename
 
-        with open(filepath, 'w') as f:
-            json.dump(bco_dmo_geojson, f, indent=4, sort_keys=True)  
- 
+        with open(filepath, "w") as f:
+            json.dump(bco_dmo_geojson, f, indent=4, sort_keys=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-    
